01a703427b04a48471766b4b9affd97cca7d3c93
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-08-12 14:08:18 +0530

    added some projects

--- src/Components/Projects/ProjectData.js
+++ src/Components/Projects/ProjectData.js
@@ -1,22 +1,23 @@
-import jobPortal from "./ProjectImg/jobportal.webp";
+
 import AiSum from "./ProjectImg/foot.png";
 import practical from "./ProjectImg/practical.png";
-import text from "./ProjectImg/text.png";
-import c2p from "./ProjectImg/c2p.png";
-import Code2pdf from "./ProjectImg/Code2pdf.png";
-import hotel from "./ProjectImg/hotel.png";
-import ChatApp from "./ProjectImg/chatapp.png";
-import audio from "./ProjectImg/audioplayr-ss.png";
-import tiny from "./ProjectImg/tiny.png";
 import ToDoFusion from "./ProjectImg/todoss.png";
+import Amazon from "./ProjectImg/Amazon.jpg"
+import Emoji from './ProjectImg/EmojiFinder.jpg'
+import StickyNote from './ProjectImg/stickynote.jpg'
+import PhoneHunting from './ProjectImg/phonehunting.jpg'
+import GymPower from './ProjectImg/gympower.jpg'
+import Aria from './ProjectImg/aria.jpg'
+import UsabilityHub from './ProjectImg/usabilityhub.jpg'
+import Gaming from './ProjectImg/gaming.jpg'
 
 export const MernProjectData = [
   {
-    img: ChatApp,
-    title: "MERN-Chat-App",
-    techstack: " React ,Node ,Express ,MongoDB ,Tailwind CSS",
-    live: "",
-    github: "",
+    img: Amazon,
+    title: "Ecommerce web app",
+    techstack: " React Js ,Tailwind CSS ",
+    live: "https://amazon-ui-clone-with-react.vercel.app/",
+    github: "https://github.com/Abhinandan411/Amazon-Clone-with-React?tab=readme-ov-file",
   },
   {
     img: ToDoFusion,
@@ -32,54 +33,54 @@ export const MernProjectData = [
     live: "",
     github: "",
   },
-    {
-    img:audio ,
-    title: "Audio player",
-    techstack: " Html ,Css ,Javascript",
-    live: "",
-    github: "",
-  },
   {
-    img: Code2pdf,
-    title: "Code2PDF",
+    img:PhoneHunting ,
+    title: "Phone Hunting",
     techstack: " Html ,CSS ,JS",
-    live: "",
-    github: "",
+    live: "https://abhinandan411.github.io/Fs-18-Weekly-Test/JavaScript%20Weekly%20Test/PhoneHunting/index.html",
+    github: "https://github.com/Abhinandan411/Fs-18-Weekly-Test/tree/main/JavaScript%20Weekly%20Test/PhoneHunting",
   },
   {
-    img: hotel,
-    title: "Hotel Website",
+    img: Emoji,
+    title: "Emoji finder",
     techstack: " Html ,CSS ,JS",
-    live: "",
-    github: "",
+    live: "https://abhinandan411.github.io/Fs-18-Assignments/JavaScript/Emoji%20Finder/index.html",
+    github: "https://github.com/Abhinandan411/Fs-18-Assignments/tree/main/JavaScript/Emoji%20Finder",
   },
   {
-    img: text,
-    title: "Text-Manipulator ",
-    techstack: " Html ,CSS ,JS ,React",
-    live: "",
-    github: "",
-  },
-  {
-    img: c2p,
-    title: "Agency Website",
+    img: StickyNote,
+    title: "Sticky Notes",
     techstack: " Html ,CSS ,JS",
-    live: "",
-    github: "",
+    live: "https://abhinandan411.github.io/Fs-18-Assignments/JavaScript/Sticky_notes/index.html",
+    github: "https://github.com/Abhinandan411/Fs-18-Assignments/tree/main/JavaScript/Sticky_notes",
   },
   {
-    img: jobPortal,
-    title: "Job Portal",
-    techstack: " Html ,CSS ,JS ",
-    live: "",
-    github: "",
+    img: GymPower,
+    title: "Gym Power",
+    techstack: " Html, Tailwind CSS",
+    live: "https://abhinandan411.github.io/Fs-18-Assignments/TailWind%20CSS%20Assignments/GYM%20website/index.html",
+    github: "https://github.com/Abhinandan411/Fs-18-Assignments/tree/main/TailWind%20CSS%20Assignments/GYM%20website",
   },
   {
-    img: tiny,
-    title: "TinyOne",
-    techstack: " Html ,CSS ,JS ",
-    live: "",
-    github: "",
+    img: Aria,
+    title: "Aria Project",
+    techstack: " Html ,CSS",
+    live: "https://abhinandan411.github.io/Fs-18-Assignments/CSS%20Assignments/Major%20Project/index.html",
+    github: "https://github.com/Abhinandan411/Fs-18-Assignments/tree/main/CSS%20Assignments/Major%20Project",
+  },
+  {
+    img: UsabilityHub,
+    title: "UsabilityHub UI clone",
+    techstack: " Html ,CSS",
+    live: "https://remarkable-pegasus-12a4e5.netlify.app/",
+    github: "https://github.com/Abhinandan411/Web-Pages_Using_HTML_CSS/tree/master/Useability_Hub_Clone",
+  },
+  {
+    img: Gaming,
+    title: "Gaming Landing page",
+    techstack: " Html ,CSS ",
+    live: "https://whimsical-custard-834fb4.netlify.app/",
+    github: "https://github.com/Abhinandan411/Web-Pages_Using_HTML_CSS/tree/master/Gaming_Landing_page",
   },
   {
     img: AiSum,
